56e6aa1d9808a11f7d34c4304ff9bbaa4b465cbb
Shinichi Ueno <uen.agile@gmail.com>
2016-11-28 11:53:00 +0900

    add file

---
+++ Biz.WebApp/Scripts/Core/Thread/Thread.ts
@@ -0,0 +1,8 @@
+﻿namespace DomBehind.Core.Thread {
+    export class Thread {
+
+        public static TimerAsync(tick: number): JQueryPromise<any> {
+            return WorkerPool.Do(TimerThread, { Message: "Start", Duration: tick });
+        }
+    }
+}
---
+++ Biz.WebApp/Scripts/Core/Thread/Timer.ts
@@ -0,0 +1,36 @@
+﻿self.onmessage = e => {
+    var arg: any = e.data;
+    if (!arg) return;
+
+    var message = arg.Message;
+
+    if (message === "Start") {
+        begin = new Date().getTime();
+        duration = arg.Duration;
+        Start();
+    } else if (message === "Reset") {
+        begin = new Date().getTime();
+    } else if (message === "Break") {
+        notifyBreak = true;
+    }
+}
+
+var begin: number
+var duration: number;
+var notifyBreak: boolean = false;
+function Start() {
+    var allow: boolean = true;
+    while (allow) {
+        // ui thread queue for onmmessage 
+        setTimeout(() => {
+            var now = new Date().getTime();
+            var end = begin + duration;
+            allow = now < end;
+        }, 1);
+
+        if (notifyBreak) return;
+    }
+    // chorme
+    var callBack: Function = self.postMessage;
+    callBack("complete");
+}
